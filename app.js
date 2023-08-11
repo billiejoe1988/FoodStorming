27aefd563ba073114bb6cb0dddf1f5a386c73bcc
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2023-08-11 18:39:51 -0300

    terminada la base de scss aplicada de forma correcta, coordinado con theme oscuro y constante al pasar de pagina en pagina

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 const themeSelect = document.getElementById("theme-select");
 
 themeSelect.addEventListener('change', (event) => {
-    const theme = event.target.value
+    const theme = event.target.value;
 
-    document.body.classList.add('animate-all')
-    document.documentElement.setAttribute('data-bs-theme', theme)
+    document.body.classList.add('animate-all');
+    document.documentElement.setAttribute('data-bs-theme', theme);
+     // Almacenar preferencia
+    localStorage.setItem('theme', theme);
+    //animacion
     setTimeout(_ => document.body.classList.remove('animate-all'), 500);
 });
+
+// Recuperar la preferencia del tema del almacenamiento local
+const preferenciaTema = localStorage.getItem('theme');
+
+// Aplicar la preferencia del tema si está almacenada
+if (preferenciaTema) {
+    document.documentElement.setAttribute('data-bs-theme', preferenciaTema);
+}
